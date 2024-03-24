34417f62a3fcd3abadb6c6ead0d7054dd37d1f96
Saka Adeyemi Shittu <zakatechsoftware@gmail.com>
2024-03-24 16:08:39 +0100

    the first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const calculate = () => {
+  let p = Number(document.getElementById("principal").value);
+  let r = Number(document.getElementById("rate").value);
+  let t = Number(document.getElementById("time").value);
+  let simpleInterest = calculateSimpleInterest(p, r, t);
+  let amount = calculateTotalPayableAmount(p, simpleInterest);
+  let result = document.getElementById("result");
+  result.innerHTML = `<div>Principal Amount: <span>${p.toFixed(2)}</span></div>
+  <div>Total Interest: <span>${simpleInterest.toFixed(2)}</span></div>
+  <div>Total Amount: <span>${amount.toFixed(2)}</span></div>`;
+};
+
+const calculateSimpleInterest = (principal, rate, time) => {
+  return (principal * rate * time) / 100;
+};
+const calculateTotalPayableAmount = (principal, interestAmount) => {
+  return principal + interestAmount;
+};
+
+if (typeof module !== "undefined")
+  module.exports = {
+    calculateSimpleInterest,
+    calculateTotalPayableAmount,
+    calculate,
+  };
